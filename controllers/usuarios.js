24db6740d528ba10161ac6df88eb256b4276bded
Maizares, Juan <juan_maizares386@hotmail.com>
2022-07-27 00:23:35 -0300

    validacion email

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,8 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
+
 const Usuario = require('../models/usuario');
+const { validationResult } = require('express-validator');
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -15,11 +18,27 @@ const usuariosGet = (req = request, res = response) => {
     });
 }
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = async(req, res = response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty() ) {
+        return res.status(400).json(errors);
+    }
 
-    const body = req.body;
-    const usuario = new Usuario(body)
+    const { nombre, correo, password, rol } = req.body;
+    const usuario = new Usuario({ nombre, correo, password, rol});
+    // verificar correo
+    const existeEmail = await Usuario.findOne({correo});
+    if (existeEmail) {
+        return res.status(400).json({
+            msg: 'Ese correo ya esta registrado'
+        });
+    }
+    // encriptar
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+    // save
     usuario.save();
+
     res.json({
         msg: 'post API - usuariosPost',
         usuario
